af827931b8bd7be0dd9896ef5f985a19e54d2b8a
Sergey <571030@mail.ru>
2022-12-03 22:52:31 +0400

    Indonesia

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+$('.carousel__wrapper').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/slide/arrow_left.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/slide/arrow_right.png"></button>',
+});
